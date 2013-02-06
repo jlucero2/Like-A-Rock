70bae8e4a991fa687722b2fc57724df00a9ee6ea
Sean Dunford <sdunford@mail.usf.edu>
2013-02-06 18:28:01 -0500

    Adding rest of the rails stuff

---
+++ app/assets/javascripts/like_a_rock.js.coffee
@@ -0,0 +1,9 @@
+window.LikeARock =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  LikeARock.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LikeARock::Application.config.secret_token = '8f3d046843c06090765e2355affd6deac831cab814dedb92225a6130e321119ce9535d67fff9edf8bcb9ddff6da081874aa945bdfab06bc4aebc64cc97d82f41'
