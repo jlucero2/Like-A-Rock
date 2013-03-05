cbe9a6d0d5d9d3d07af5a6edcf85a11fc232996c
Deivis Largo <dlargo67@gmail.com>
2013-03-04 20:41:14 -0500

    migrate files

--- db/migrate/20130303000342_create_images.rb
+++ db/migrate/20130303000342_create_images.rb
@@ -3,8 +3,10 @@ class CreateImages < ActiveRecord::Migration
     create_table :images do |t|
       t.string :url
       t.integer :sol
+      t.references :album
 
       t.timestamps
     end
+    add_index :images, :album_id
   end
 end
