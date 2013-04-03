4b48800106d40400660bf1d392fd43bd0126630a
jaluce <ja.luce2@gmail.com>
2013-04-03 14:32:05 +0000

    added earthday to the album model, I am not replacing sol though.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
-  attr_accessible :url, :timestamp, :sol, :num_images
-  validates :sol, :presence => true, :uniqueness => true
+  attr_accessible :url, :timestamp, :sol, :num_images, :earthday
+#  validates :earthday, :sol, :presence => true, :uniqueness => true
   validates :num_images, :presence => true 
   has_many :images
 end
