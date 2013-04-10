ab42856d0a6212c4ac7790e5cbaf078cda816309
Ryan Kohnenkamp <rkohnenk@mail.usf.edu>
2013-04-09 20:03:12 -0400

    Now the admin/sign_up page redirects to root instead of buggin out.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :registerable,
+  # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
