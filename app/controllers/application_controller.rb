8ecd314adb0c26bf0b224e18355c4a6170166e41
Sean Dunford <sdunford@mail.usf.edu>
2013-04-21 21:06:47 -0400

    Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   prepend_before_filter :application
 
   def application
+     @latestResponses = Response.order('created_at DESC').all(:limit => 3)
     if !admin_signed_in? && !user_signed_in?
       if User.where(:name => "Guest" + request.remote_ip).empty?
         user = User.new
