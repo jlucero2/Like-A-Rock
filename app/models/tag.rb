f6f7ab18cb2cf03711b67f6d930a13aeb2d5b498
Deivis Largo <dlargo67@gmail.com>
2013-04-23 18:06:38 -0400

    changes presence

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
-  belongs_to :image, :presence => true
+  belongs_to :image
   attr_accessible :x, :y, :name
 end
